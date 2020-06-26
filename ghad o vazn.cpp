625248523444b7c5f496f1bb07273f11e75a7597
Reza Moradi <rezamoradi83r@gmail.com>
2020-06-27 00:00:23 +0430

    first commit

---
+++ ghad o vazn.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+#include<string>
+using namespace std;
+
+main(){
+	int height;
+	int weight;
+	cout<<"Ghadet ro begoo : ";
+	cin>>height;
+	cout<<"Vaznet ro begoo :";
+	cin>>weight;
+	int vazn = height - 2-100;
+	cout<<"Vazn E'dealet :"<<vazn<<"\n";
+	int ezafe = (weight)-vazn;
+	cout<<ezafe<<"Kg Ezafe\\Kam Dari!";
+	
+}
